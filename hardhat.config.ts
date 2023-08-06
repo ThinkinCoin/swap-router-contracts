dd6ec5303094c931726b11771694d926e03f7f0a
Mauricio Zanetti Filho <contact@thinkincoin.com>
2023-08-06 09:39:11 -0300

    false again

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,7 +10,7 @@ const DEFAULT_COMPILER_SETTINGS = {
   settings: {
     evmVersion: 'istanbul',
     optimizer: {
-      enabled: true,
+      enabled: false,
       runs: 10_000,
     },
     metadata: {
