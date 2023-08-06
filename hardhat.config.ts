d0ab4e6d5b311d23af818d1d5263a5b24cafc133
Mauricio Zanetti Filho <contact@thinkincoin.com>
2023-08-06 10:28:30 -0300

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,5 @@
-import '@nomiclabs/hardhat-ethers'
-import "@nomicfoundation/hardhat-verify";
+import "@nomicfoundation/hardhat-ethers"
+import "@nomicfoundation/hardhat-verify"
 import '@nomiclabs/hardhat-waffle'
 import 'hardhat-typechain'
 import 'hardhat-watcher'
